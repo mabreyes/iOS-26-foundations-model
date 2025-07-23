bf18b4ab6ab4b34854e611c71b78e835326ecdc8
Marc Reyes <hi@marcr.xyz>
2025-07-23 13:13:11 +0800

    Initial Commit

---
+++ ios-foundations/ios_foundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_foundationsApp.swift
+//  ios-foundations
+//
+//  Created by Marc on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_foundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
